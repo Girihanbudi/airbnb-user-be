382fb462b58d3e3a0f5a40d8703c73a92b8fe868
girihanbudi <Ghanbudi@gmail.com>
2023-03-04 21:30:03 +0700

    added migration setup

---
+++ db/migration/migrateup.go
@@ -0,0 +1,19 @@
+package migrate
+
+import (
+	"airbnb-user-be/internal/app/locale"
+	orm "airbnb-user-be/internal/pkg/gorm"
+	"airbnb-user-be/internal/pkg/log"
+
+	"gorm.io/gorm"
+)
+
+func MigrateUp(db gorm.DB) {
+	models := []interface{}{
+		&locale.Region{},
+	}
+
+	if err := db.AutoMigrate(models); err != nil {
+		log.Fatal(orm.Instance, "failed to run migration", err)
+	}
+}
