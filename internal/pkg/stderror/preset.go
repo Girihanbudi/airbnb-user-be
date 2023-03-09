106cc1519577c467f476777ec6950571f1d5489f
girihanbudi <Ghanbudi@gmail.com>
2023-03-09 22:39:31 +0700

    fixed standard error preset

--- internal/pkg/stderror/preset.go
+++ internal/pkg/stderror/preset.go
@@ -3,6 +3,7 @@ package stderror
 import "net/http"
 
 var (
-	DEF_AUTH_401 = New(http.StatusUnauthorized, "DEF_AUTH_401", "Failed to authorize user")
-	DEF_DATA_400 = New(http.StatusBadRequest, "DEF_DATA_400", "Failed to bind request")
+	DEF_SERVER_500 = New(http.StatusUnauthorized, "DEF_SERVER_500", "Failed to get message")
+	DEF_AUTH_401   = New(http.StatusUnauthorized, "DEF_AUTH_401", "Failed to authorize user")
+	DEF_DATA_400   = New(http.StatusBadRequest, "DEF_DATA_400", "Failed to bind request")
 )
