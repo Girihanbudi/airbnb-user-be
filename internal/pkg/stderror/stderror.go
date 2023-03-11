d9c967303210b53c7d2c4fddf9aeea3b47285a46
girihanbudi <Ghanbudi@gmail.com>
2023-03-11 23:53:16 +0700

    updated standard error

--- internal/pkg/stderror/stderror.go
+++ internal/pkg/stderror/stderror.go
@@ -1,22 +1,34 @@
 package stderror
 
+import (
+	"errors"
+	"fmt"
+)
+
 type StdError struct {
-	HttpCode      int     `json:"-"`
-	InternalCode  string  `json:"internalCode"`
-	Message       string  `json:"message"`
-	Error         error   `json:"-"`
-	ErrorMessages *string `json:"error,omitempty"`
+	HttpCode     int    `json:"-"`
+	InternalCode string `json:"internalCode"`
+	Error        error  `json:"-"`
 }
 
-func New(code int, internalCode string, message string) StdError {
+func New(code int, internalCode string, errMsg ...any) StdError {
+	var format string
+	for i, _ := range errMsg {
+		if i == 0 {
+			format = format + "%s"
+		} else {
+			format = format + " %s"
+		}
+	}
+
 	return StdError{
 		HttpCode:     code,
 		InternalCode: internalCode,
-		Message:      message,
+		Error:        fmt.Errorf(format, errMsg...),
 	}
 }
 
 func (err *StdError) ErrorMsg(message error) *StdError {
-	err.Error = message
+	err.Error = errors.New(message.Error())
 	return err
 }
