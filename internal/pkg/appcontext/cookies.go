98dbec6b83650c9934c20f25ebda3434782e2f84
girihanbudi <Ghanbudi@gmail.com>
2023-03-15 23:04:33 +0700

    added comment

--- internal/pkg/appcontext/cookies.go
+++ internal/pkg/appcontext/cookies.go
@@ -1,7 +1,7 @@
 package appcontext
 
 const (
-	LocaleCode     = "locale"
-	LocaleDuration = 60 * 60 * 24
-	LocaleDefault  = "en-US"
+	LocaleCode     = "locale"         // locale application context code
+	LocaleDuration = 60 * 60 * 24 * 7 // expires in a week
+	LocaleDefault  = "en-US"          // default language using English US
 )
