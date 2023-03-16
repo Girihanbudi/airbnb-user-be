d383e0201c66ad4fc51120d184908df329b39ed3
girihanbudi <Ghanbudi@gmail.com>
2023-03-16 23:50:45 +0700

    fixed local CORS issue for reading cookie

--- internal/pkg/http/server/router/setting.go
+++ internal/pkg/http/server/router/setting.go
@@ -1,14 +1,24 @@
 package router
 
 import (
+	"airbnb-user-be/internal/pkg/env"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func CORSSetting() gin.HandlerFunc {
+
+	var origin string
+	if env.Stage(env.CONFIG.Stage) == env.StageLocal {
+		origin = env.CONFIG.LocalServer
+	} else {
+		origin = env.CONFIG.Domain
+	}
+
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("SameSite", "None")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, OPTIONS, PATCH")
