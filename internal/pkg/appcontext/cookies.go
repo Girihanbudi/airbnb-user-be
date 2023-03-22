d743d20d62cf92a80fb26c21d1403dcb68c6ca99
girihanbudi <Ghanbudi@gmail.com>
2023-03-22 16:07:13 +0700

    refactor cookies duration

--- internal/pkg/appcontext/cookies.go
+++ internal/pkg/appcontext/cookies.go
@@ -1,13 +1,20 @@
 package appcontext
 
+const day = 60 * 60 * 24
+
 const (
-	LocaleCode     = "locale"         // locale application context code
-	LocaleDuration = 60 * 60 * 24 * 7 // expires in a week
-	LocaleDefault  = "en-US"          // default language using English US
+	LocaleCode     = "locale" // locale application context code
+	LocaleDuration = 7 * day  // expires in a week
+	LocaleDefault  = "en-US"  // default language using English US
 )
 
 const (
-	CurrencyCode     = "currency"       // currency application context code
-	CurrencyDuration = 60 * 60 * 24 * 7 // expires in a week
-	CurrencyDefault  = "USD"            // default currency using United State Dollar
+	CurrencyCode     = "currency" // currency application context code
+	CurrencyDuration = 7 * day    // expires in a week
+	CurrencyDefault  = "USD"      // default currency using United State Dollar
+)
+
+const (
+	OauthCode     = "oauth"   // oauth application context code
+	OauthDuration = 365 * day // expires in a week
 )
