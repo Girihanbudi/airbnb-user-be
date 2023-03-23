63f665af5cd8c886227963567077a4b41b53e73e
girihanbudi <Ghanbudi@gmail.com>
2023-03-23 22:09:31 +0700

    added jwt and cache config

--- internal/pkg/env/config/config.go
+++ internal/pkg/env/config/config.go
@@ -1,8 +1,10 @@
 package config
 
 import (
+	cache "airbnb-user-be/internal/pkg/cache/config"
 	gorm "airbnb-user-be/internal/pkg/gorm/config"
 	httpserver "airbnb-user-be/internal/pkg/http/server/config"
+	jwt "airbnb-user-be/internal/pkg/jwt/config"
 	oauth "airbnb-user-be/internal/pkg/oauth/config"
 )
 
@@ -13,4 +15,6 @@ type Config struct {
 	HttpServer httpserver.Config `mapstructure:"httpserver"`
 	DB         gorm.Config       `mapstructure:"db"`
 	Oauth      oauth.Config      `mapstructure:"oauth"`
+	Jwt        jwt.Config        `mapstructure:"jwt"`
+	Cache      cache.Config      `mapstructure:"cache"`
 }
