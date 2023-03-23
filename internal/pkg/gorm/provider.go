bffdb86b559bf8d5bb1cc84e0d6cb459a5559442
girihanbudi <Ghanbudi@gmail.com>
2023-03-23 21:44:25 +0700

    merge initiation function to provider

--- internal/pkg/gorm/provider.go
+++ internal/pkg/gorm/provider.go
@@ -1,10 +1,15 @@
 package gorm
 
 import (
+	"airbnb-user-be/internal/pkg/env"
 	"airbnb-user-be/internal/pkg/gorm/config"
+	"airbnb-user-be/internal/pkg/log"
 	"context"
+	"fmt"
 
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 const Instance string = "ORM"
@@ -23,5 +28,30 @@ func NewORM(config config.Config) *Engine {
 	}
 
 	engine.InitConnection()
+	log.Event(Instance, fmt.Sprintf("connected to %s:%s", env.CONFIG.DB.Host, env.CONFIG.DB.Name))
 	return &engine
+}
+
+func (g *Engine) InitConnection() {
+	dsn := fmt.Sprintf(`host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s`,
+		g.Host,
+		g.User,
+		g.Password,
+		g.Name,
+		g.Port,
+		g.SslMode,
+		g.Timezone,
+	)
+
+	var config gorm.Config
+	if env.CONFIG.Stage == string(env.StageLocal) {
+		config.Logger = logger.Default.LogMode(logger.Info)
+	}
+
+	db, err := gorm.Open(postgres.Open(dsn), &config)
+	if err != nil {
+		log.Fatal(Instance, "failed to init db connection", err)
+	}
+
+	g.DB = db
 }
--- internal/pkg/http/server/router/provider.go
+++ internal/pkg/http/server/router/provider.go
@@ -2,10 +2,23 @@ package router
 
 import (
 	"airbnb-user-be/internal/pkg/env"
-	"airbnb-user-be/internal/pkg/http/server/middleware/cookie"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
+
+func DefaultCORSSetting() gin.HandlerFunc {
+
+	return cors.New(cors.Config{
+		AllowOrigins: env.CONFIG.Origins,
+		AllowMethods: []string{"GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
+		AllowHeaders: []string{"Origin", "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization", "accept", "Cache-Control", "X-Requested-With"},
+		// ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	})
+}
 
 func NewRouter() *gin.Engine {
 	if env.CONFIG.Stage == string(env.StageLocal) {
@@ -16,12 +29,6 @@ func NewRouter() *gin.Engine {
 
 	router := gin.New()
 	router.Use(gin.Recovery())
-
-	// GIN apply CORS setting
-	router.Use(CORSSetting())
-
-	// GIN bind all cookie
-	router.Use(cookie.BindAll())
 
 	return router
 }
