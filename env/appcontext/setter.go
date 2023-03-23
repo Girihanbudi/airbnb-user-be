4aa07f6e13e9b7b09dcf91c30ee852b2536a981e
girihanbudi <Ghanbudi@gmail.com>
2023-03-23 23:33:56 +0700

    added context setter default

--- env/appcontext/setter.go
+++ env/appcontext/setter.go
@@ -10,3 +10,8 @@ func SetFromGinRouter(ctx *gin.Context, key any, val any) {
 	newCtx := context.WithValue(ctx.Request.Context(), key, val)
 	ctx.Request = ctx.Request.WithContext(newCtx)
 }
+
+func SetFromDefaultRouter(ctx *context.Context, key any, val any) {
+	newCtx := context.WithValue(*ctx, key, val)
+	ctx = &newCtx
+}
