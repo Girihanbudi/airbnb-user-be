ecee4d66de5986da8085cae46460e4d2afb6a8ad
girihanbudi <Ghanbudi@gmail.com>
2023-03-25 17:36:37 +0700

    created app context getter

---
+++ env/appcontext/getter.go
@@ -0,0 +1,29 @@
+package appcontext
+
+import "context"
+
+func GetLocale(ctx context.Context) string {
+	value := ctx.Value(LocaleCode)
+	if value == nil {
+		return LocaleDefault
+	}
+	return value.(string)
+}
+
+func GetCurrency(ctx context.Context) string {
+	value := ctx.Value(CurrencyCode)
+	if value == nil {
+		return CurrencyDefault
+	}
+	return value.(string)
+}
+
+func GetAccessToken(ctx context.Context) *string {
+	value := ctx.Value(AccessTokenCode)
+	if value == nil {
+		return nil
+	}
+	token := value.(string)
+
+	return &token
+}
