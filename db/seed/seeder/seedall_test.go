458e5735a85a102e30c9c3705f3319e830a13021
girihanbudi <Ghanbudi@gmail.com>
2023-03-25 10:04:36 +0700

    added all seeder runner

---
+++ db/seed/seeder/seedall_test.go
@@ -0,0 +1,37 @@
+package seeder
+
+import (
+	"airbnb-user-be/internal/pkg/env"
+	"testing"
+
+	"airbnb-user-be/internal/pkg/gorm"
+)
+
+func TestSeedAll(t *testing.T) {
+	env.InitEnv(envConfig)
+	config := env.ProvideEnv().DB
+	engine := gorm.NewORM(config)
+	db := *engine.DB
+
+	t.Log("seeding error translation...")
+	if err := SeedErrTranslation(db); err != nil {
+		t.Error("failed to seed error translation", err)
+	}
+
+	t.Log("seeding locale...")
+	if err := SeedLocale(db); err != nil {
+		t.Error("failed to seed locale", err)
+	}
+
+	t.Log("seeding currency...")
+	if err := SeedCurrency(db); err != nil {
+		t.Error("failed to seed currency", err)
+	}
+
+	t.Log("seeding currency translation...")
+	if err := SeedCurrencyTranslation(db); err != nil {
+		t.Error("failed to seed currency translation", err)
+	}
+
+	t.Log("finish seeding")
+}
