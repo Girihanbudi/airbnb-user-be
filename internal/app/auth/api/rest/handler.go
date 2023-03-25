3c833447c8dc62c3d1746276bd3c6e9a790d9492
girihanbudi <Ghanbudi@gmail.com>
2023-03-25 17:38:00 +0700

    removed comment

--- internal/app/auth/api/rest/handler.go
+++ internal/app/auth/api/rest/handler.go
@@ -8,16 +8,6 @@ func (h Handler) ContinueWithGoogle(ctx *gin.Context) {
 	h.Auth.ContinueWithGoogle(*ctx)
 }
 
-// func (r Router) ContinueWithEmail(c *gin.Context) {
-// 	var req request.SignIn
-// 	err := c.BindJSON(&req)
-// 	if err != nil {
-
-// 	}
-
-// 	r.Auth.SignIn(c, req)
-// }
-
 func (h Handler) OauthGoogleCallback(ctx *gin.Context) {
 
 	h.Auth.OauthGoogleCallback(*ctx)
