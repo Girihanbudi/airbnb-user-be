700d3cff547b542dbb34ad4dbda91a06ef027307
girihanbudi <Ghanbudi@gmail.com>
2023-04-10 15:40:43 +0700

    refactor config and added otp cache

--- internal/pkg/cache/config/config.go
+++ internal/pkg/cache/config/config.go
@@ -1,9 +1,12 @@
 package config
 
-import (
-	auth "airbnb-user-be/internal/pkg/cache/auth/config"
-)
+type CacheDefaultConfig struct {
+	Host     string `mapstructure:"host"`
+	Password string `mapstructure:"password"`
+	Db       int    `mapstructure:"db"`
+}
 
 type Config struct {
-	Auth auth.Config `mapstructure:"auth"`
+	Auth CacheDefaultConfig `mapstructure:"auth"`
+	Otp  CacheDefaultConfig `mapstructure:"otp"`
 }
