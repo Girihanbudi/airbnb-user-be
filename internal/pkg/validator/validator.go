99580fd5281bd3f05dfe508fd60a5575bccf0b19
girihanbudi <Ghanbudi@gmail.com>
2023-04-10 21:01:51 +0700

    updated validator
    - move initiation to it own module
    - added date_iso8601 validation

--- internal/pkg/validator/validator.go
+++ internal/pkg/validator/validator.go
@@ -1,6 +1,7 @@
 package validator
 
 import (
+	"regexp"
 	"sync"
 
 	goValidator "github.com/go-playground/validator/v10"
@@ -12,13 +13,19 @@ var (
 	validator *goValidator.Validate
 )
 
-func InitValidator() {
+func init() {
 	// do only once
 	singleValidator.Do(func() {
 		validator = goValidator.New()
+		validator.RegisterValidation("date_iso8601", ValidateDateIso8601)
 	})
 }
 
 func GetValidator() *goValidator.Validate {
 	return validator
+}
+
+func ValidateDateIso8601(fl goValidator.FieldLevel) bool {
+	var re = regexp.MustCompile(`^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$`)
+	return re.MatchString(fl.Field().String())
 }
