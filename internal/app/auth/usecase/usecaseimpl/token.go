77be544e912e926b82a38c9c8c9b394ad98932ea
girihanbudi <Ghanbudi@gmail.com>
2023-04-11 23:55:23 +0700

    updated auth token
    - updated error code
    - added otp token handler

--- internal/app/auth/usecase/usecaseimpl/token.go
+++ internal/app/auth/usecase/usecaseimpl/token.go
@@ -5,6 +5,8 @@ import (
 	errpreset "airbnb-user-be/internal/app/auth/preset/error"
 	transutil "airbnb-user-be/internal/app/translation/util"
 	authcache "airbnb-user-be/internal/pkg/cache/auth"
+	otpcache "airbnb-user-be/internal/pkg/cache/otp"
+	"airbnb-user-be/internal/pkg/codegenerator"
 	"airbnb-user-be/internal/pkg/env"
 	"airbnb-user-be/internal/pkg/jwt"
 	"airbnb-user-be/internal/pkg/stderror"
@@ -17,25 +19,25 @@ func (u Usecase) createAndStoreTokensPair(ctx gin.Context, userId string) (err *
 	clientLocale := appcontext.GetLocale(reqCtx)
 	at, claims, createAtErr := jwt.GenerateToken(appcontext.AccessTokenDuration, nil)
 	if createAtErr != nil {
-		err = transutil.TranslateError(reqCtx, errpreset.AUTH_GET_400, clientLocale)
+		err = transutil.TranslateError(reqCtx, errpreset.TknGenerateFailed, clientLocale)
 		return
 	}
 
 	storeAtErr := authcache.Set(claims["jti"].(string), userId, appcontext.AccessTokenDuration)
 	if storeAtErr != nil {
-		err = transutil.TranslateError(reqCtx, errpreset.AUTH_GET_502, clientLocale)
+		err = transutil.TranslateError(reqCtx, errpreset.TknStoreFailed, clientLocale)
 		return
 	}
 
 	rt, claims, createRtErr := jwt.GenerateToken(appcontext.RefreshTokenDuration, nil)
 	if createRtErr != nil {
-		err = transutil.TranslateError(reqCtx, errpreset.AUTH_GET_400, clientLocale)
+		err = transutil.TranslateError(reqCtx, errpreset.TknGenerateFailed, clientLocale)
 		return
 	}
 
 	storeRtErr := authcache.Set(claims["jti"].(string), userId, appcontext.RefreshTokenDuration)
 	if storeRtErr != nil {
-		err = transutil.TranslateError(reqCtx, errpreset.AUTH_GET_502, clientLocale)
+		err = transutil.TranslateError(reqCtx, errpreset.TknStoreFailed, clientLocale)
 		return
 	}
 
@@ -69,6 +71,20 @@ func (u Usecase) createAndStoreTokensPair(ctx gin.Context, userId string) (err *
 		false,
 		false,
 	)
+
+	return
+}
+
+func (u Usecase) createAndStoreOtp(ctx gin.Context, userId string) (otp string, err *stderror.StdError) {
+	reqCtx := ctx.Request.Context()
+	clientLocale := appcontext.GetLocale(reqCtx)
+
+	otp = codegenerator.RandomEncodedNumbers(6)
+	storeOtpErr := otpcache.Set(otp, userId, appcontext.OtpDuration)
+	if storeOtpErr != nil {
+		err = transutil.TranslateError(reqCtx, errpreset.TknStoreFailed, clientLocale)
+		return
+	}
 
 	return
 }
