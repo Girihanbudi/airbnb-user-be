6a804c48b81bf41287c2a56e99e69e52b255a868
girihanbudi <Ghanbudi@gmail.com>
2023-04-12 23:18:45 +0700

    updated error code format

--- internal/pkg/stderror/preset.go
+++ internal/pkg/stderror/preset.go
@@ -1,9 +1,7 @@
 package stderror
 
-import "net/http"
-
 var (
-	DEF_SERVER_500 = New(http.StatusUnauthorized, "DEF_SERVER_500", "Failed to get message")
-	DEF_AUTH_401   = New(http.StatusUnauthorized, "DEF_AUTH_401", "Failed to authorize user")
-	DEF_DATA_400   = New(http.StatusBadRequest, "DEF_DATA_400", "Failed to bind request")
+	DEF_SERVER_500 = "DEF_SERVER_500"
+	DEF_AUTH_401   = "DEF_AUTH_401"
+	DEF_DATA_400   = "DEF_DATA_400"
 )
