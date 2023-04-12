d8b360968fc98db0a302ad5526183c7b60ea0636
girihanbudi <Ghanbudi@gmail.com>
2023-04-12 23:19:03 +0700

    fixed default error

--- internal/app/translation/util/translateerr.go
+++ internal/app/translation/util/translateerr.go
@@ -4,12 +4,17 @@ import (
 	"airbnb-user-be/internal/app/translation/repo/repoimpl"
 	"airbnb-user-be/internal/pkg/stderror"
 	"context"
+	"net/http"
+)
+
+var (
+	defaultErr = stderror.New(http.StatusInternalServerError, "DEF_SERVER_500", "Failed to get translation")
 )
 
 func TranslateError(ctx context.Context, code, localeCode string) (err *stderror.StdError) {
 	trans, getTransErr := repoimpl.ErrTranslationRepo.GetErrTranslation(ctx, code, localeCode)
 	if getTransErr != nil {
-		err = &stderror.DEF_SERVER_500
+		err = &defaultErr
 		return
 	}
 	newErr := stderror.New(trans.HttpCode, trans.Code, trans.Message)
