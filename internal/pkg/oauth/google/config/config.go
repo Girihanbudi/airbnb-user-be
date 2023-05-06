d776e81bdcab39fa6220405288edab6e8c95e189
girihanbudi <Ghanbudi@gmail.com>
2023-05-06 23:50:01 +0700

    removed unused package

--- internal/pkg/oauth/google/config/config.go
+++
@@ -1,9 +0,0 @@
-package config
-
-type Config struct {
-	ClientId     string   `mapstructure:"clientid"`
-	ClientSecret string   `mapstructure:"clientsecret"`
-	UserInfoApi  string   `mapstructure:"userinfoapi"`
-	RedirectUrl  string   `mapstructure:"redirecturl"`
-	Scopes       []string `mapstructure:"scopes"`
-}
