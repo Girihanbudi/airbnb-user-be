3b64f43952e564b4195de95f3fa3851686f3d591
girihanbudi <Ghanbudi@gmail.com>
2023-05-16 23:09:21 +0700

    added json metadata

--- internal/app/middleware/elastic/preset.go
+++ internal/app/middleware/elastic/preset.go
@@ -8,26 +8,26 @@ import (
 )
 
 type Log struct {
-	ProcessTime time.Duration
-	Request     Request
-	Response    Response
+	ProcessTime time.Duration `json:"process_time"`
+	Request     Request       `json:"request"`
+	Response    Response      `json:"response"`
 }
 
 type Request struct {
-	Time      time.Time
-	Method    string
-	Uri       string
-	Proto     string
-	UserAgent string
-	Referer   string
-	Body      string
-	Ip        string
+	Time      time.Time `json:"time"`
+	Method    string    `json:"method"`
+	Uri       string    `json:"uri"`
+	Proto     string    `json:"proto"`
+	UserAgent string    `json:"user_agent"`
+	Referer   string    `json:"referer"`
+	Body      string    `json:"body"`
+	Ip        string    `json:"ip"`
 }
 
 type Response struct {
-	Time       time.Time
-	StatusCode int
-	Body       string
+	Time       time.Time `json:"time"`
+	StatusCode int       `json:"status_code"`
+	Body       string    `json:"body"`
 }
 
 type GinLogWriter struct {
